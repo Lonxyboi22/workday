bf69b3fb8006646aa5c5d236341002f6b40fa699
Lonxyboi <drewthemachine22@gmail.com>
2022-08-12 08:43:27 -0400

    I wasn't very pleased with how the code was so i redid it :)

--- Develop/script.js
+++ Develop/script.js
@@ -1,57 +1,66 @@
-// Display today's day and date
-var todayDate = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(todayDate);
+/**
+ * DEFINE VARIABLES
+ */
 
-$(document).ready(function () {
-    // saveBtn click listener 
-    $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+ var saveBtn = $(".saveBtn");
 
-        // Save text in local storage
-        localStorage.setItem(time, text);
-    })
-   
-    function timeTracker() {
-        //get current number of hours.
-        var timeNow = moment().hour();
-
-        // loop over time blocks
-        $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-            // To check the time and add the classes for background indicators
-            if (blockTime < timeNow) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-            }
-            else if (blockTime === timeNow) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-
-            }
-        })
-    }
-
-    // Get item from local storage if any
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
-
-    timeTracker();
-})
+ /**
+  * FUNCTIONS
+  */
+ 
+ // current day is displayed at the top of the calendar
+ $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+ 
+ // each time block is color-coded to indicate whether it is in the past, present, or future
+ function timeBlockColor() {
+     var hour = moment().hours();
+ 
+     $(".time-block").each(function() {
+         var currHour = parseInt($(this).attr("id"));
+ 
+         // console.log(this); //each time-block
+ 
+         if (currHour > hour) {
+             $(this).addClass("future");
+         } else if (currHour === hour) {
+             $(this).addClass("present");
+         } else {
+             $(this).addClass("past");
+         }
+     })
+ };
+ 
+//save button click function
+ saveBtn.on("click", function() {
+ 
+     // console.log(this); //save button
+     var time = $(this).siblings(".hour").text();
+     var plan = $(this).siblings(".plan").val();
+ 
+     // THEN the text for that event is saved in local storage
+     localStorage.setItem(time, plan);
+ });
+ 
+ // WHEN I refresh the page
+ // THEN the saved events persist
+ function usePlanner() {
+ 
+     $(".hour").each(function() {
+         var currHour = $(this).text();
+         var currPlan = localStorage.getItem(currHour);
+ 
+         // console.log(this);
+         // console.log(currHour);
+ 
+         if(currPlan !== null) {
+             $(this).siblings(".plan").val(currPlan);
+         }
+     });
+ }
+ 
+ /**
+  * CALL FUNCTIONS
+  */
+ 
+ timeBlockColor();
+ usePlanner();
